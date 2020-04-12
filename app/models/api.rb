27f6ad695fb3d701a49e6803a5985d9692eb4c18
vpk11 <devvpk@gmail.com>
2020-04-12 17:23:14 +0530

    Code refactoring

--- app/models/api.rb
+++ app/models/api.rb
@@ -9,4 +9,12 @@ class Api < ApplicationRecord
   has_many :responses
 
   METHODS = %w[GET POST PATCH PUT DELETE].freeze
+
+  def self.init(params)
+    Api.new(
+      method: params[:method], end_point: params[:end_point],
+      description: params[:description], version_id: params[:version_id],
+      archived: params[:archived]
+    )
+  end
 end
