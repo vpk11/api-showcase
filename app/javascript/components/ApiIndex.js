25b1884bd4c34de1bcd4ad9931f8a5d21c4f2149
Voulf <akshaypsoman@Akshays-MacBook-Air-1591.local>
2020-04-27 20:04:19 +0530

    created version/show page to display all apis under a project version

--- app/javascript/components/ApiIndex.js
+++ app/javascript/components/ApiIndex.js
@@ -7,7 +7,6 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import CardDeck from 'react-bootstrap/CardDeck'
-import ShowCaseListItem from './ShowCaseListItem'
 
 class ApiIndex extends React.Component {
     render() {
