42171dcace8fb9a433139323eb2c98aadcb859e5
vpk11 <devvpk@gmail.com>
2020-05-02 21:25:06 +0530

    Signup and Login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,15 +4,22 @@
 class SessionsController < ApplicationController
   skip_before_action :authorized, only: %i[new create welcome]
 
+  # Render Login Form
   def new; end
 
+  # Login Happens Here
   def create
     @user = User.find_by(email: params[:email])
     if @user&.authenticate(params[:password])
-      sessions[:user_id] = @user.id
+      session[:user_id] = @user.id
       redirect_to '/welcome'
     else
-      redirect_to '/login'
+      @error = if @user
+                 'Wrong Password'
+               else
+                 "User doesn't exist. Try SignUp!"
+               end
+      render :new
     end
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,20 @@
 
 # Users Controller
 class UsersController < ApplicationController
-  skip_before_action :authorized, only: [:new, :create]
+  skip_before_action :authorized, only: %i[new create]
 
+  # SignUp form
+  def new; end
+
+  # SignUp - Create User
   def create
-    @user = User.create(params.require(:user).permit(:username,
-                                                     :password))
-    session[:user_id] = @user.id
-    redirect_to '/welcome'
+    @user = User.new(name: params[:name], email: params[:email],
+                     password: params[:password])
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to '/welcome'
+    else
+      render :new
+    end
   end
 end
