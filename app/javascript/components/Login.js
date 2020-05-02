c74a2461be3cb412492d28ade02a649ab8f20d07
vpk11 <devvpk@gmail.com>
2020-05-02 21:25:06 +0530

    Signup and Login

--- app/javascript/components/Login.js
+++ app/javascript/components/Login.js
@@ -4,10 +4,31 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import ShowcaseNav from './ShowcaseNav'
+import CsrfInput from './CsrfInput'
+import DismissibleAlert from './DismissibleAlert'
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      alert: {
+        alertType: props.alertType,
+        showAlert: props.showAlert,
+        alertMessage: props.alertMessage,
+      }
+    }
+    this.onCloseAlert = this.onCloseAlert.bind(this);
+  }
 
   componentDidMount() {
     document.getElementsByClassName("container-fluid")[0].parentElement.style.width = '100%';
+  }
+
+  onCloseAlert() {
+    this.setState({
+      alert: {
+        ...this.state.alert, ...{ showAlert: false }
+      }
+    })
   }
 
   render() {
@@ -24,31 +45,36 @@ class Login extends React.Component {
             <Container style={containerMarginStyle}>
               <Row>
                 <Col>
+                  {this.state.alert.showAlert && <DismissibleAlert type={this.state.alert.alertType} alertHeading={`🦄 ${this.state.alert.alertMessage}`} onClose={this.onCloseAlert} />}
                   <div className="auth-wrapper">
                     <div className="auth-inner">
-                      <form>
+                      <form action='/login' method='POST'>
                         <h3>Sign In</h3>
 
                         <div className="form-group">
                           <label>Email address</label>
-                          <input type="email" className="form-control" placeholder="Enter email" />
+                          <input name='email' type="email" className="form-control" placeholder="Enter email" />
                         </div>
 
                         <div className="form-group">
                           <label>Password</label>
-                          <input type="password" className="form-control" placeholder="Enter password" />
+                          <input name="password" type="password" className="form-control" placeholder="Enter password" />
                         </div>
 
                         <div className="form-group">
                           <div className="custom-control custom-checkbox">
-                            <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                            <input name='remember' type="checkbox" className="custom-control-input" id="customCheck1" />
                             <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
                           </div>
                         </div>
+                        <CsrfInput />
 
                         <button type="submit" className="btn btn-primary btn-block">Submit</button>
                         <p className="forgot-password text-right">
-                          Forgot <a href="#">password?</a>
+                          <a href="#">Forgot password?</a>
+                        </p>
+                        <p className="forgot-password text-right">
+                          <a href="/signup">Sign Up!</a>
                         </p>
                       </form>
                     </div>
@@ -61,6 +87,12 @@ class Login extends React.Component {
       </Container>
     );
   }
+}
+
+Login.propTypes = {
+  alertType: PropTypes.string,
+  showAlert: PropTypes.bool,
+  alertMessage: PropTypes.string,
 }
 
 export default Login
--- app/javascript/components/SignUp.js
+++ app/javascript/components/SignUp.js
@@ -4,10 +4,32 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import ShowcaseNav from './ShowcaseNav'
+import CsrfInput from './CsrfInput'
+import DismissibleAlert from './DismissibleAlert'
 class SignUp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      alert: {
+        alertType: props.alertType,
+        showAlert: props.showAlert,
+        alertMessage: props.alertMessage,
+        alertList: props.alertList,
+      }
+    }
+    this.onCloseAlert = this.onCloseAlert.bind(this);
+  }
 
   componentDidMount() {
     document.getElementsByClassName("container-fluid")[0].parentElement.style.width = '100%';
+  }
+
+  onCloseAlert() {
+    this.setState({
+      alert: {
+        ...this.state.alert, ...{ showAlert: false }
+      }
+    })
   }
 
   render() {
@@ -24,34 +46,32 @@ class SignUp extends React.Component {
             <Container style={containerMarginStyle}>
               <Row>
                 <Col>
+                  {this.state.alert.showAlert && <DismissibleAlert type={this.state.alert.alertType} alertHeading={`🦄 ${this.state.alert.alertMessage}`} alertList={this.state.alert.alertList} onClose={this.onCloseAlert} />}
                   <div className="auth-wrapper">
                     <div className="auth-inner">
-                      <form>
+                      <form action='/signup' method='POST'>
                         <h3>Sign Up</h3>
 
                         <div className="form-group">
-                          <label>First name</label>
-                          <input type="text" className="form-control" placeholder="First name" />
-                        </div>
-
-                        <div className="form-group">
-                          <label>Last name</label>
-                          <input type="text" className="form-control" placeholder="Last name" />
+                          <label>Full name</label>
+                          <input name='name' type="text" className="form-control" placeholder="First name" required />
                         </div>
 
                         <div className="form-group">
                           <label>Email address</label>
-                          <input type="email" className="form-control" placeholder="Enter email" />
+                          <input name='email' type="email" className="form-control" placeholder="Enter email" required />
                         </div>
 
                         <div className="form-group">
                           <label>Password</label>
-                          <input type="password" className="form-control" placeholder="Enter password" />
+                          <input name='password' type="password" className="form-control" placeholder="Enter password" required />
                         </div>
+                        <input type="hidden" name="account_id" value={this.props.account_id} />
+                        <CsrfInput />
 
                         <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
                         <p className="forgot-password text-right">
-                          Already registered <a href="#">sign in?</a>
+                          Already registered <a href="/login">sign in?</a>
                         </p>
                       </form>
                     </div>
@@ -64,6 +84,13 @@ class SignUp extends React.Component {
       </Container >
     );
   }
+}
+
+SignUp.propTypes = {
+  account_id: PropTypes.number,
+  showAlert: PropTypes.bool,
+  alertMessage: PropTypes.string,
+  alertList: PropTypes.array,
 }
 
 export default SignUp
