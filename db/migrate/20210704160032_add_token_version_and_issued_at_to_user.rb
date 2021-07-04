8c431165c2140814d8a3a49a2a5325857c30faec
anbk <ahammednafihbk@gmail.com>
2021-07-05 01:03:47 +0530

    :sparkles: Jwt access and refresh tokens

---
+++ db/migrate/20210704160032_add_token_version_and_issued_at_to_user.rb
@@ -0,0 +1,6 @@
+class AddTokenVersionAndIssuedAtToUser < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :token_version, :integer, default: 1
+    add_column :users, :token_issued_at, :datetime
+  end
+end
